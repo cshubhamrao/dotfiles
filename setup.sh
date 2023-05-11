8a55fb749960ea84b729cf67037aa4291da4603d
Shubham Rao <shubham@nimblebox.ai>
2023-05-11 21:25:41 +0530

    chore:  more symlinks

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
 ln -s $(realpath ./.zshrc) ~/.zshrc
+ln -s $(realpath ./antigen.zsh) ~/antigen.zsh
+ln -s $(realpath ./.p10k.zsh) ~/.p10k.zsh
 ln -s $(realpath .config/nvim) ~/.config/nvim
 
